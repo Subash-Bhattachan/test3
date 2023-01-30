af5d7555d0c54ff4085783bb165f4695d90897d9
Subash-Bhattachan <73665341+Subash-Bhattachan@users.noreply.github.com>
2023-01-30 00:13:44 -0500

    Update variable.tf

--- VnetPeering/variable.tf
+++ VnetPeering/variable.tf
@@ -17,6 +17,6 @@ variable "hubtospoke" {
 }
 
 variable "subscription_ID" {
-type = string
-default = "3e7f8f26-7ad0-456e-a365-05294930c3e5"
+    type = string
+    default = "3e7f8f26-7ad0-456e-a365-05294930c3e5"
 }
