dd70e97e5ed40a315ee21fbc6679d692df976b11
Subash-Bhattachan <73665341+Subash-Bhattachan@users.noreply.github.com>
2023-01-29 22:22:23 -0500

    Update backend.tf

--- VnetPeering/backend.tf
+++ VnetPeering/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "pfe-rg-ido-dev-lcs-sa-tf-eastus-001"
-    storage_account_name = "azurelcsvmtfbackends"
-    container_name       = "lcs-tf-state-6878629e-8533-4d23-9175-205f2ac842e2"
-    key                  = "lcsvm-ido-peering.tfstate"
-    subscription_id      = "6878629e-8533-4d23-9175-205f2ac842e2"
+    resource_group_name  = "subashtfstates"
+    storage_account_name = "subashstoragetf"
+    container_name       = "subashcontainer"
+    key                  = "vnetpeering.tfstate"
+    subscription_id      = "3e7f8f26-7ad0-456e-a365-05294930c3e5"
   }
 }
