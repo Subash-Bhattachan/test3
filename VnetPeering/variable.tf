1cab5e0f815e6c143ee5bb6e3d998acacd3ea453
Subash-Bhattachan <73665341+Subash-Bhattachan@users.noreply.github.com>
2023-01-31 18:36:58 -0500

    Update variable.tf

--- VnetPeering/variable.tf
+++ VnetPeering/variable.tf
@@ -1,33 +1,6 @@
-variable "rgname" {
-    type = string
-    description = "Name of resource group"
-    default = "subash-rg"
-}
-
-variable "location" {
-    type = string
-    description = "Azure location of resource group"
-    default = "eastus"
-}
-
-variable "hubtospoke" {
-    type = map
-    description = "A list of keys and values"
-    
-}
-
-variable "subscription_ID" {
-    type = string
-    default = "3e7f8f26-7ad0-456e-a365-05294930c3e5"
-}
-
-
-
-
-
 variable "subscription_ID" {
 type = string
-default = "8811f127-9372-42c2-8faf-2f02664e32ab"
+default = "3e7f8f26-7ad0-456e-a365-05294930c3e5"
 }
 
 variable "hubtospoke" {
