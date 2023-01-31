36ad0a440a60d913a5d1f5a582f24a3943a85235
Subash-Bhattachan <73665341+Subash-Bhattachan@users.noreply.github.com>
2023-01-31 18:21:52 -0500

    Update variable.tf

--- modules/VnetPeering/variable.tf
+++ modules/VnetPeering/variable.tf
@@ -1,10 +1,39 @@
-variable "rgname" {
+variable "peering1_name" {
     type = string
-    description = "Name of resource group"
+    description = "Name of peering name"
 }
 
-variable "location" {
+variable "rgname_vnet1" {
     type = string
-    description = "Azure location of resource group"
-    default = "eastus"
+    description = "VNET1 resource group name"
+}
+
+variable "vnet1_name" {
+    type = string
+    description = "VNET1 name"
+}
+
+variable "remote_vnet2_id" {
+    type = string
+    description = "VNET1 id"
+}
+
+variable "peering2_name" {
+    type = string
+    description = "Name of peering name"
+}
+
+variable "rgname_vnet2" {
+    type = string
+    description = "VNET1 resource group name"
+}
+
+variable "vnet2_name" {
+    type = string
+    description = "VNET1 name"
+}
+
+variable "remote_vnet1_id" {
+    type = string
+    description = "VNET1 id"
 }
