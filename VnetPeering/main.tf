ecd505008bdd42510a45f55aa80283f4456d1204
Subash-Bhattachan <73665341+Subash-Bhattachan@users.noreply.github.com>
2023-01-31 18:19:50 -0500

    Update main.tf

--- VnetPeering/main.tf
+++ VnetPeering/main.tf
@@ -1,9 +1,15 @@
-resource "azurerm_resource_group" "resourcegroup" {
-    name                     = var.rgname
-    location                 = var.location
-    tags = {
-        PrimaryOwner = "Matthew.J.Santacroce@pfizer.com"
-        SecondaryOwner = "thomas.wielachowski@pfizer.com"
-        CostCenterID = "00000000"
-    }
+# Create Virtual Network Peering
+
+module "peering" {
+  for_each   =   var.hubtospoke
+  source    = "../modules/vnetpeering"
+  peering1_name = each.value.peering1_name
+  rgname_vnet1 = each.value.rgname_vnet1
+  vnet1_name = each.value.vnet1_name
+  remote_vnet2_id = each.value.remote_vnet2_id
+  peering2_name = each.value.peering2_name
+  rgname_vnet2 = each.value.rgname_vnet2
+  vnet2_name = each.value.vnet2_name
+  remote_vnet1_id = each.value.remote_vnet1_id
 }
+
