87775f963e7b872b70e5d833cf50f42d8f4a1f3c
Subash-Bhattachan <73665341+Subash-Bhattachan@users.noreply.github.com>
2023-01-31 18:40:41 -0500

    Update main.tf

--- VnetPeering/main.tf
+++ VnetPeering/main.tf
@@ -3,10 +3,12 @@
 module "peering" {
   for_each   =   var.hubtospoke
   source    = "../modules/vnetpeering"
+  
   peering1_name = each.value.peering1_name
   rgname_vnet1 = each.value.rgname_vnet1
   vnet1_name = each.value.vnet1_name
   remote_vnet2_id = each.value.remote_vnet2_id
+  
   peering2_name = each.value.peering2_name
   rgname_vnet2 = each.value.rgname_vnet2
   vnet2_name = each.value.vnet2_name
