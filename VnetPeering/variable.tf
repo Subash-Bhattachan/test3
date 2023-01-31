4bb9d3b68ecb99f6351868fbe8fe48191faeed69
Subash-Bhattachan <73665341+Subash-Bhattachan@users.noreply.github.com>
2023-01-31 18:34:30 -0500

    Update variable.tf

--- VnetPeering/variable.tf
+++ VnetPeering/variable.tf
@@ -20,3 +20,18 @@ variable "subscription_ID" {
     type = string
     default = "3e7f8f26-7ad0-456e-a365-05294930c3e5"
 }
+
+
+
+
+
+variable "subscription_ID" {
+type = string
+default = "8811f127-9372-42c2-8faf-2f02664e32ab"
+}
+
+variable "hubtospoke" {
+  type        = map(any)
+  description = "(Required) Virtual Network peering"
+}
+
